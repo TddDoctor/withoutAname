628b270bb1d11f92c0cc47a4bafc3e2dfcfb172b
TddDoctor <ishmael@mountbatten.net>
2014-12-08 08:38:17 +0300

    added user create cept

---
+++ tests/acceptance/devtracUsercreationCept.php
@@ -0,0 +1,14 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Create users with different roles');
+$I->click('Sign in');
+$I->dontsee('Enter your Devtrac Test The Tests username.');
+$I->see('Enter your Devtrac Test username');
+$I->see('Enter the password that accompanies your username.');
+$I->fillField('name','admin');
+$I->fillField('pass','admin');
+$I->click('Log in');
+$I->dontsee('Sorry, unrecognized username or password');
+$I->dontseeLink('Have you forgotten your password?');
+$I->see('Welcome, admin');
+$I->amGoingTo('submit user edit form/page');
